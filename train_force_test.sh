9867b621aaba449c3e5e638729d36d4d05ff9f39
wqw <nathan.wuqw@gmail.com>
2025-12-09 13:30:46 +0800

    update upload logic of model

--- train_force_test.sh
+++ train_force_test.sh
@@ -2,10 +2,12 @@
 set -e
 
 # 只让当前进程看到 4,5,6,7 号 GPU
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=2,3
 
 # 切到工程根目录
 cd /raid/qiweiw/workspace/T2-VLA
+
+#uv run scripts/compute_norm_stats.py --config-name pi0_libero_force_low_mem_finetune
 
 uv run scripts/train.py pi0_libero_force_low_mem_finetune \
   --exp-name force_test2 \
